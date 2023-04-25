c524e00582fce7e55ff30e906bc2434196302b15
MehKad <mehkadiri@gmail.com>
2023-04-25 11:38:47 +0000

    edit working

--- KeySkills/screens/Profile.js
+++ KeySkills/screens/Profile.js
@@ -6,24 +6,68 @@ import {
   Image,
   StatusBar,
   Modal,
+  TextInput,
 } from "react-native";
 import moment from "moment";
 import React, { Component } from "react";
 import { onLogOut } from "../components/Functions";
-
 import { connect } from "react-redux";
+import { AntDesign, FontAwesome5 } from "@expo/vector-icons";
+import firebase from "firebase/compat";
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+  }
   state = {
-    modalVisible: true,
+    modalVisible: false,
+    posting: true,
+    prevName: "",
+    prevPhone: "",
+    prevEmail: "",
+    prevImage: null,
+    prevDate: "",
   };
 
   toggleModal = () => {
-    this.setState({ modalVisible: !this.state.modalVisible });
+    const { currentUser } = this.props;
+    const date = currentUser.dateBirth;
+    const prevDate = moment(date.seconds * 1000).format("DD/MM/YYYY");
+    this.setState({
+      prevName: currentUser.fullName,
+      prevPhone: currentUser.phone,
+      prevEmail: currentUser.email,
+      prevImage: currentUser.profilePic,
+      prevDate: prevDate,
+      modalVisible: !this.state.modalVisible,
+    });
+    console.log(this.state.prevDate);
+  };
+
+  handleSaveChanges = () => {
+    const { currentUser } = this.props;
+    const uid = firebase.auth().currentUser.uid;
+    const { prevEmail, prevName, prevPhone, prevImage } = this.state;
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(uid)
+      .update({
+        fullName: prevName,
+        phone: prevPhone,
+        email: prevEmail,
+        profilePic: prevImage,
+      })
+      .then(() => {
+        this.setState({ posting: false });
+        this.toggleModal();
+        console.log("user succesfully modified");
+      });
   };
 
   render() {
     const { currentUser } = this.props;
+    const { prevImage, prevEmail, prevName, prevPhone } = this.state;
     return (
       <View style={styles.container}>
         <View style={styles.top}>
@@ -60,15 +104,56 @@ class Profile extends Component {
           </TouchableOpacity>
         </View>
 
-        <Modal
-          visible={this.state.modalVisible}
-          transparent
-          animationType="slide"
-        >
-          <View style={styles.modalContainer}>
-            <Text>Edit Profile</Text>
-            <TouchableOpacity onPress={this.toggleModal}>
-              <Text style={styles.closeButtonText}>Close</Text>
+        <Modal visible={this.state.modalVisible} animationType="slide">
+          <View style={styles.container}>
+            <AntDesign
+              name="close"
+              size={24}
+              onPress={this.toggleModal}
+              style={styles.closeIcon}
+            />
+            <View style={styles.content}>
+              <View style={{ alignItems: "center" }}>
+                <TouchableOpacity>
+                  {prevImage && (
+                    <Image source={{ uri: prevImage }} style={styles.image} />
+                  )}
+                  {!prevImage && (
+                    <Image
+                      source={require("../assets/profile_placeholder.png")}
+                      style={styles.image}
+                    />
+                  )}
+                </TouchableOpacity>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Name"
+                  value={prevName}
+                  onChangeText={(prevName) => this.setState({ prevName })}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Phone"
+                  value={prevPhone}
+                  onChangeText={(prevPhone) => this.setState({ prevPhone })}
+                  maxLength={10}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="email"
+                  value={prevEmail}
+                  keyboardType="email-address"
+                  onChangeText={(prevEmail) => this.setState({ prevEmail })}
+                />
+              </View>
+            </View>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={this.handleSaveChanges}
+            >
+              <Text>Save</Text>
             </TouchableOpacity>
           </View>
         </Modal>
@@ -144,15 +229,55 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     padding: 20,
     width: "90%",
-    height: "90%",
+    height: "75%",
     alignSelf: "center",
-    marginTop: "10%",
+    marginTop: "20%",
     borderWidth: 1,
   },
   closeButtonText: {
     color: "#386BF6",
     fontWeight: "bold",
     marginTop: 20,
+  },
+  saveButton: {
+    backgroundColor: "#386BF6",
+    borderRadius: 15,
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    marginTop: 20,
+    alignItems: "center",
+  },
+  saveButtonText: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+  closeIcon: {
+    alignSelf: "flex-end",
+  },
+  input: {
+    borderWidth: 2,
+    padding: 8,
+    width: 330,
+    borderColor: "rgba(232, 236, 244, 1)",
+    borderRadius: 8,
+    marginTop: "5%",
+  },
+  button: {
+    backgroundColor: "#004AAD",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#fff",
+    paddingBottom: 15,
+    paddingTop: 15,
+    paddingLeft: 120,
+    paddingRight: 120,
+    marginTop: "5%",
+    marginBottom: "10%",
   },
 });
 
