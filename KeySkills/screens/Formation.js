53e290fcd5d4bf5b699935637f662b06c0c062aa
MehKad <mehkadiri@gmail.com>
2023-04-28 11:15:38 +0000

    delete formation + style V2

--- KeySkills/screens/Formation.js
+++ KeySkills/screens/Formation.js
@@ -14,6 +14,9 @@ import firebase from "firebase/compat";
 import FirstModal from "../components/firstModal";
 import SecondModal from "../components/secondModal";
 
+import { AnimatedFAB, TextInput } from "react-native-paper";
+import { AntDesign } from "@expo/vector-icons";
+
 class Formation extends Component {
   state = {
     Name: [],
@@ -21,6 +24,8 @@ class Formation extends Component {
     firstMod: false,
     secondMod: false,
     dourous: {},
+    addMod: false,
+    newForm: "",
   };
 
   componentDidMount() {
@@ -33,15 +38,17 @@ class Formation extends Component {
   handleSecond = () => {
     this.setState((prevState) => ({ secondMod: !prevState.secondMod }));
   };
+  handleFirstAdd = () => {
+    this.setState((prevState) => ({ addMod: !prevState.addMod }));
+  };
 
   fetchFormation = () => {
     return firebase
       .firestore()
       .collection("formation")
       .onSnapshot((snapshot) => {
-        snapshot.docs.map((formation) => {
-          this.setState({ Name: [...this.state.Name, formation.id] });
-        });
+        const formationNames = snapshot.docs.map((formation) => formation.id);
+        this.setState({ Name: formationNames });
       });
   };
 
@@ -50,12 +57,10 @@ class Formation extends Component {
     firebase
       .firestore()
       .collection("Lessons")
+      .where(title, "==", true)
       .onSnapshot((snapshot) => {
-        snapshot.docs.map((test) => {
-          if (test.data()[title]) {
-            this.setState({ Lessons: [...this.state.Lessons, test.id] });
-          }
-        });
+        const lessonNames = snapshot.docs.map((lesson) => lesson.id);
+        this.setState({ Lessons: lessonNames });
       });
     this.handleFirst();
   };
@@ -77,12 +82,33 @@ class Formation extends Component {
     this.handleSecond();
   };
 
+  addFormation = () => {
+    firebase
+      .firestore()
+      .collection("formation")
+      .doc(this.state.newForm)
+      .set({ name: this.state.newForm })
+      .then(() => this.handleFirstAdd());
+  };
+
+  deleteF = (title) => {
+    firebase
+      .firestore()
+      .collection("formation")
+      .doc(title)
+      .delete()
+      .then(() => console.log("deleted ", title));
+  };
+
   render() {
-    const { Name, Lessons, firstMod, secondMod, dourous } = this.state;
+    const { Name, Lessons, firstMod, secondMod, dourous, addMod, newForm } =
+      this.state;
     const { currentUser } = this.props;
     return (
       <View style={styles.container}>
-        <Text>Formation : </Text>
+        <View style={styles.header}>
+          <Text style={styles.title}>Formation : </Text>
+        </View>
         <View style={styles.lescours}>
           {Name.map((title) => (
             <TouchableOpacity
@@ -90,11 +116,29 @@ class Formation extends Component {
               style={styles.small}
               onPress={() => this.fetchLessons(title)}
             >
-              <Text style={{ color: "white" }}>{title}</Text>
+              <Text style={{ color: "white" }} key={title}>
+                {title}
+              </Text>
+              <AntDesign
+                name="delete"
+                size={24}
+                onPress={() => this.deleteF(title)}
+              />
             </TouchableOpacity>
           ))}
         </View>
-        {currentUser.admin ? console.log("admin") : console.log("not")}
+        {currentUser.admin && (
+          <AnimatedFAB
+            icon={"plus"}
+            label={"Add"}
+            onPress={() => {
+              this.handleFirstAdd();
+            }}
+            animateFrom={"right"}
+            iconMode={"dynamic"}
+            style={styles.fabStyle}
+          />
+        )}
         <FirstModal
           showModal={firstMod}
           closeModal={this.handleFirst}
@@ -106,6 +150,45 @@ class Formation extends Component {
           closeSecondModal={this.handleSecond}
           dourous={dourous}
         />
+        <Modal
+          visible={addMod}
+          animationType="slide"
+          transparent={true}
+          onRequestClose={() => {
+            this.handleFirstAdd();
+          }}
+        >
+          <TouchableOpacity
+            style={styles.background}
+            onPress={() => {
+              this.handleFirstAdd();
+            }}
+          >
+            <View style={styles.addmod}>
+              <AntDesign
+                name="close"
+                size={24}
+                onPress={() => {
+                  this.handleFirstAdd();
+                }}
+                style={{ alignSelf: "flex-end", marginBottom: 10 }}
+              />
+              <TextInput
+                placeholder="New Formation"
+                maxLength={50}
+                onChangeText={(text) => {
+                  this.setState({ newForm: text });
+                }}
+              />
+              <TouchableOpacity
+                style={styles.save}
+                onPress={() => this.addFormation()}
+              >
+                <Text style={{ color: "white" }}>Add</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        </Modal>
       </View>
     );
   }
@@ -117,24 +200,33 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
   },
+  header: {
+    width: "100%",
+    height: "10%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 40,
+  },
   lescours: {
     flex: 1,
     width: "100%",
     flexDirection: "row",
-    justifyContent: "space-evenly",
+    justifyContent: "space-between",
     flexWrap: "wrap",
     marginTop: 20,
   },
   small: {
     backgroundColor: "red",
-    borderRadius: 25,
-    alignItems: "center",
-    justifyContent: "center",
+    borderRadius: 10,
     paddingLeft: 20,
     paddingRight: 20,
     margin: 15,
     width: "40%",
-    aspectRatio: 1,
+    height: "10%",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
   },
   modalContainer: {
     flex: 1,
@@ -144,6 +236,33 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginVertical: 20,
+  },
+  fabStyle: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+  },
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    backgroundColor: "rgba(0,0,0,0.2)",
+  },
+  addmod: {
+    backgroundColor: "white",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    height: "35%",
+    padding: 20,
+  },
+  save: {
+    backgroundColor: "blue",
+    borderRadius: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 40,
+    paddingRight: 40,
+    marginTop: 20,
+    alignSelf: "center",
   },
 });
 
