aca3e97ecfdcd8634dfd99b8b586fd1df55ea80f
MehKad <mehkadiri@gmail.com>
2023-05-12 22:51:19 +0000

    update home

--- KeySkills/screens/Home.js
+++ KeySkills/screens/Home.js
@@ -1,3 +1,4 @@
+import React, { Component, useState, useEffect } from "react";
 import {
   Text,
   StyleSheet,
@@ -7,36 +8,24 @@ import {
   Image,
   Modal,
 } from "react-native";
-import React, { Component } from "react";
-
 import { connect } from "react-redux";
-
 import { AntDesign } from "@expo/vector-icons";
 import { fetchGcUsers } from "../redux/actions";
-
 import firebase from "firebase/compat";
 
-class Home extends Component {
-  state = {
-    showModal: false,
-    selectedFormation: null,
-    users: [],
-  };
+function Home(props) {
+  const { currentUser, lessonsAdmin, dispatch, users: propsUsers } = props;
+  const [showModal, setShowModal] = useState(false);
+  const [selectedFormation, setSelectedFormation] = useState(null);
+  const [users, setUsers] = useState([]);
 
-  handleFormationPress = (formation) => {
-    const { dispatch } = this.props;
-    this.setState({ selectedFormation: formation, showModal: true });
+  const handleFormationPress = (formation) => {
+    setSelectedFormation(formation);
+    setShowModal(true);
     dispatch(fetchGcUsers(formation));
   };
 
-  componentDidUpdate(prevProps) {
-    if (this.props.users !== prevProps.users) {
-      this.setState({ users: this.props.users });
-    }
-  }
-
-  removeUser(id) {
-    const { selectedFormation } = this.state;
+  const removeUser = (id) => {
     firebase
       .firestore()
       .collection("Lessons")
@@ -45,18 +34,15 @@ class Home extends Component {
       .doc(id)
       .delete()
       .then(() => {
-        this.setState((prevState) => ({
-          users: prevState.users.filter((user) => user.id !== id),
-        }));
+        setUsers(users.filter((user) => user.id !== id));
         console.log("User deleted successfully");
       })
       .catch((error) => {
         console.error(error);
       });
-  }
+  };
 
-  verify(id) {
-    const { selectedFormation } = this.state;
+  const verify = (id) => {
     firebase
       .firestore()
       .collection("Lessons")
@@ -67,9 +53,8 @@ class Home extends Component {
         confirmed: true,
       })
       .then(() => {
-        // update the users array in the state to reflect the change
-        this.setState((prevState) => ({
-          users: prevState.users.map((user) => {
+        setUsers(
+          users.map((user) => {
             if (user.id === id) {
               return {
                 ...user,
@@ -81,16 +66,16 @@ class Home extends Component {
             } else {
               return user;
             }
-          }),
-        }));
-        console.log("updated successfully");
+          })
+        );
+        console.log("Updated successfully");
       })
       .catch((error) => {
         console.error(error);
       });
-  }
-  remove(id) {
-    const { selectedFormation } = this.state;
+  };
+
+  const remove = (id) => {
     firebase
       .firestore()
       .collection("Lessons")
@@ -101,8 +86,8 @@ class Home extends Component {
         confirmed: false,
       })
       .then(() => {
-        this.setState((prevState) => ({
-          users: prevState.users.map((user) => {
+        setUsers(
+          users.map((user) => {
             if (user.id === id) {
               return {
                 ...user,
@@ -114,34 +99,37 @@ class Home extends Component {
             } else {
               return user;
             }
-          }),
-        }));
-        console.log("updated successfully");
+          })
+        );
+        console.log("Updated successfully");
       })
       .catch((error) => {
         console.error(error);
       });
-  }
+  };
 
-  render() {
-    const { currentUser, lessonsAdmin } = this.props;
-    const { selectedFormation, showModal, users } = this.state;
-    console.log(users);
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Image source={{ uri: currentUser.profilePic }} style={styles.img} />
-          <Text style={styles.title}>Welcome back {currentUser.fullName}</Text>
-        </View>
+  useEffect(() => {
+    if (propsUsers !== users) {
+      setUsers(propsUsers);
+    }
+  }, [propsUsers]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={{ uri: currentUser.profilePic }} style={styles.img} />
+        <Text style={styles.title}>Welcome back {currentUser.fullName}</Text>
+      </View>
+      {currentUser.admin && (
         <View style={styles.lescours}>
           <Text style={styles.desc}>
-            Click on One of the following Lessons to see it's users :
+            Click on One of the following Lessons to see its users:
           </Text>
           {lessonsAdmin.map((title) => (
             <TouchableOpacity
               key={title}
               style={styles.small}
-              onPress={() => this.handleFormationPress(title)}
+              onPress={() => handleFormationPress(title)}
             >
               <Text style={{ color: "white" }} key={title}>
                 {title}
@@ -149,60 +137,50 @@ class Home extends Component {
             </TouchableOpacity>
           ))}
         </View>
-        <Modal visible={showModal} animationType="slide">
-          <View style={styles.modalContainer}>
-            <AntDesign
-              name="close"
-              size={24}
-              onPress={() => this.setState({ showModal: false })}
-              style={{ alignSelf: "flex-end", marginBottom: 10 }}
-            />
-            <Text style={styles.modalTitle}>{selectedFormation}</Text>
-            <Text
-              style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}
-            >
-              User List
-            </Text>
-            {users.map((user) => (
-              <View
-                key={user.id}
-                style={{
-                  width: "90%",
-                  padding: 20,
-                  borderWidth: 1,
-                  borderRadius: 8,
-                  marginBottom: 20,
-                  flexDirection: "row",
-                  justifyContent: "space-evenly",
-                }}
-              >
-                <Text style={{ fontSize: 15 }}>{user.data.fullName}</Text>
-                <AntDesign
-                  name="delete"
-                  size={24}
-                  onPress={() => this.removeUser(user.id)}
-                />
+      )}
+      <Modal visible={showModal} animationType="slide">
+        <View style={styles.modalContainer}>
+          <AntDesign
+            name="close"
+            size={24}
+            onPress={() => setShowModal(false)}
+            style={{ alignSelf: "flex-end", marginBottom: 10 }}
+          />
+          <Text style={styles.modalTitle}>{selectedFormation}</Text>
+          <Text style={styles.modalSubtitle}>User List</Text>
+          {users.map((user) => (
+            <View key={user.id} style={styles.userContainer}>
+              <Text style={styles.userName}>{user.data.fullName}</Text>
+              <View style={styles.userActions}>
+                <TouchableOpacity
+                  onPress={() => removeUser(user.id)}
+                  style={styles.userAction}
+                >
+                  <AntDesign name="delete" size={24} color="white" />
+                </TouchableOpacity>
                 {!user.data.confirmed && (
-                  <AntDesign
-                    name="checksquare"
-                    size={24}
-                    onPress={() => this.verify(user.id)}
-                  />
+                  <TouchableOpacity
+                    onPress={() => verify(user.id)}
+                    style={[styles.userAction, styles.userActionCancel]}
+                  >
+                    <AntDesign name="close" size={24} color="white" />
+                  </TouchableOpacity>
                 )}
                 {user.data.confirmed && (
-                  <AntDesign
-                    name="close"
-                    size={24}
-                    onPress={() => this.remove(user.id)}
-                  />
+                  <TouchableOpacity
+                    onPress={() => remove(user.id)}
+                    style={[styles.userAction, styles.userActionConfirm]}
+                  >
+                    <AntDesign name="checksquare" size={24} color="white" />
+                  </TouchableOpacity>
                 )}
               </View>
-            ))}
-          </View>
-        </Modal>
-      </View>
-    );
-  }
+            </View>
+          ))}
+        </View>
+      </Modal>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -215,55 +193,84 @@ const styles = StyleSheet.create({
     flex: 0.5,
     width: "100%",
     flexDirection: "row",
-    justifyContent: "space-between",
+    alignItems: "center",
+    justifyContent: "space-evenly",
     flexWrap: "wrap",
   },
   small: {
-    backgroundColor: "red",
-    borderRadius: 5,
-    paddingLeft: 20,
-    paddingRight: 20,
-    margin: 15,
+    backgroundColor: "#212121",
+    padding: 20,
+    borderRadius: 10,
     width: "40%",
-    height: "15%",
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-  },
-  title: {
-    fontSize: 20,
-    alignSelf: "center",
-    padding: 10,
-    paddingTop: 25,
-    fontFamily: "serif",
-    fontWeight: "bold",
-  },
-  img: {
-    width: 50,
-    height: 50,
-    borderRadius: 100,
-    margin: 20,
+    margin: 10,
   },
   header: {
     flexDirection: "row",
     alignItems: "center",
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  img: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  title: {
+    fontFamily: "Roboto",
+    fontSize: 20,
+    fontWeight: "bold",
   },
   desc: {
-    fontSize: 15,
-    textAlign: "justify",
+    fontSize: 16,
+    marginBottom: 10,
     fontWeight: "bold",
-    fontFamily: "Roboto",
-    padding: 20,
   },
   modalContainer: {
     flex: 1,
     alignItems: "center",
+    marginTop: StatusBar.currentHeight,
   },
   modalTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  modalSubtitle: {
     fontSize: 20,
     fontWeight: "bold",
+    alignSelf: "flex-start",
+    marginLeft: "5%",
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  userContainer: {
+    width: "90%",
+    padding: 20,
+    borderWidth: 1,
+    borderRadius: 8,
     marginBottom: 20,
-    fontFamily: "Roboto",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  userName: {
+    fontSize: 15,
+  },
+  userActions: {
+    flexDirection: "row",
+  },
+  userAction: {
+    marginLeft: 10,
+    backgroundColor: "#212121",
+    padding: 10,
+    borderRadius: 5,
+  },
+  userActionConfirm: {
+    backgroundColor: "green",
+  },
+  userActionCancel: {
+    backgroundColor: "red",
   },
 });
 
